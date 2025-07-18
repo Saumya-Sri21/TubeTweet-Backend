c3b0ccccb18cf38a0c6a9184dc018e0400bdd060
Saumya Srivastava <saumyasrivastava498@gmail.com>
2025-07-18 20:20:36 +0530

    Update user.routes.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -11,25 +11,21 @@ const router = express.Router();
 // Simple multer setup to store file in "uploads" folder
 const upload = multer({ dest: 'uploads/' });
 
-//route for register
+
 router.post(           
   '/register',
-  upload.fields([                          // Apply fields for avatar and coverImage
+  upload.fields([                         
     { name: 'avatar', maxCount: 1 },
     { name: 'coverImage', maxCount: 1 }
   ]),
   registerUser
 );
 
-//route for login
+
 router.route("/login").post(loginUser)
 
-//secure routes=routes that work only if logged in
+router.route("/logout").post(verifyJWT,logOutUser)  
 
-//route for logout (secure routes)
-router.route("/logout").post(verifyJWT,logOutUser)   //next use kra tha in verifyJWT taki next logOutUser p vhla jae controller
-
-//route to refresh access token(secured route)
 router.route("/Clickhere").post(refreshAccessToken)
 
 export default router;
